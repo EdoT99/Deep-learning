58e9e7e4bd6319c358c90ffeeb29e1c5330330c4
edoardo.taccaliti <97667070+EdoT99@users.noreply.github.com>
2024-11-12 22:50:16 +0100

    Create builder_model.py

---
+++ builder_model.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn 
+
+
+
+class DeepHand0(nn.Module):
+
+  def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__():
+
+
+
+
+  def forward(self, x: torch.Tensor):
