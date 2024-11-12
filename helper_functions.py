3124482def63a12c3e5a15f47747494a327222cd
edoardo.taccaliti <97667070+EdoT99@users.noreply.github.com>
2024-11-12 09:29:41 +0100

    Create helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,20 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+#----plotting loss-functions------
+
+def plot_losses(loss_values_train,loss_values_test,epochs_count):
+
+  plt.figure(figsize=(16, 8))
+  # Plot training and test loss curves
+  plt.plot(epochs_count, loss_values_train, label="train loss")
+  plt.plot(epochs_count, loss_values_test, label="test loss")
+
+  plt.title('Training and Test Loss Curves')
+  plt.ylabel('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+  plt.show()
+
+
